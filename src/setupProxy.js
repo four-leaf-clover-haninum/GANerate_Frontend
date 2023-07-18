15299f72891d4143fab9b89bae750c8bd0d90fc0
sefdcrxe <128684050+sefdcrxe@users.noreply.github.com>
2023-07-19 01:34:14 +0900

    로그인 회원가입

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function(app){
     app.use(
         '/api',
         proxy({
-            target : 'http://localhost:8080',
+            target : 'http://localhost:3000',
             changeOrigin: true,
         })
     );
