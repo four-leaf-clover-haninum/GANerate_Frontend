25ef4caa151e969764ef16642345a4d4f0606e1e
sefdcrxe <128684050+sefdcrxe@users.noreply.github.com>
2023-08-15 00:39:53 +0900

    상품 등록 페이지 구성

--- src/components/views/UploadProductPage/UploadProductPage.js
+++ src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
 import { Typography, Button, Form, Input } from 'antd';
 import FileUpload from '../../utils/FileUpload';
+import { Navbar as CustomNavbar, Nav } from 'react-bootstrap';
 import Axios from 'axios';
+import { FaUserCircle, FaDownload } from 'react-icons/fa';
+
 const { TextArea } = Input;
 
 const Continents = [
@@ -75,9 +78,43 @@ function UploadProductPage(props) {
 
 
     return (
+
+        <div className="HomePage d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+        <div style={{ justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        
+            {/* 네비 바 */}
+            <React.Fragment>
+            <CustomNavbar bg="light" variant="light" className="static-top" style={{ padding: '10px 20px', height: '80px' }}>
+              <div className="container">
+                <a className="navbar-brand" href="/HomePage" style={{ fontWeight: '900', fontSize: '35px', color: '#BB2649' }}>
+                  <strong>GAN:ERATE</strong>
+                </a>
+  
+          {/* 링크 추가 및 라우팅 설정 */}
+          <Nav className="me-auto">
+            <Nav.Link href="/" style={{ fontSize: '22px', padding: '0 60px' }}>이용 안내</Nav.Link>
+            <Nav.Link href="/" style={{ fontSize: '22px', padding: '0 60px' }}>데이터 생성</Nav.Link>
+            <Nav.Link href="/" style={{ fontSize: '22px', padding: '0 60px' }}>데이터 마켓</Nav.Link>
+            <Nav.Link href="/" style={{ fontSize: '22px', padding: '0 60px' }}>데이터 검색</Nav.Link>
+          </Nav>
+  
+          {/* 로그인 및 마이페이지 링크 추가 및 라우팅 설정 */}
+          <Nav className="ml-auto">
+      <Nav.Link href="/auth/signin" className="btn btn-1" style={{ fontSize: '20px', color: 'white' }}>Log In</Nav.Link>
+      <Nav.Link href="/MyPage" className="btn btn-2 ml-2" style={{ fontSize: '20px', color: 'white', marginLeft: '20px' }}>
+        <FaUserCircle style={{ fontSize: '24px', marginRight: '10px' }} />
+        My Page
+      </Nav.Link>
+      </Nav>
+  
+        </div>
+      </CustomNavbar>
+      </React.Fragment>
+
+
         <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
             <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
-                <h2> 상품 업로드</h2>
+                <h1> 상품 업로드</h1>
             </div>
 
             <Form onSubmit={submitHandler}>
@@ -111,6 +148,8 @@ function UploadProductPage(props) {
             </Form>
 
 
+        </div>
+        </div>
         </div>
     )
 }
